6324ac6d8caf0656e3ff99ceb004c20dfe4e47ba
PristenCityMan <timonovsv1@mail.ru>
2024-07-30 18:10:32 +0300

    конец вторника

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+async def read_root():
+    return {"message": "Hello world!"}
+
+
+@app.get("/")
+def home():
+    return {"message": "This message!", "content": None}
+
+
+@app.get("/html/")
+def html():
+    return """<h1>Hello world From HTML</h1>"""
